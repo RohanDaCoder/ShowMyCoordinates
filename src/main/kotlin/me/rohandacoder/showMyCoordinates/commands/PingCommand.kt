9d59817bfc9e39b08a772d37cecb068f16a86376
Rohan <rohanbmgo2010@gmail.com>
2024-11-02 21:23:55 +0600

    u

--- src/main/kotlin/me/rohandacoder/showMyCoordinates/commands/PingCommand.kt
+++ src/main/kotlin/me/rohandacoder/showMyCoordinates/commands/PingCommand.kt
@@ -4,7 +4,7 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 
-class PingCommand : CommandExecutor{
+class PingCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
         sender.sendMessage("[Show My Coordinates] Pong!")
         return false
